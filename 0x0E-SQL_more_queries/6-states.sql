232595b77c78e64677d214c6e4b02bc68693ac73
vergilx6x <med.work96@hotmail.com>
2024-05-22 15:03:50 -0700

    task_6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,2 @@
+-- Creates the database hbtn_0d_usa and the table 'states' in the database 'hbtn_0d_usa' on my MySQL server.
+CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
