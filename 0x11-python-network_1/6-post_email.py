498fcfc4885027bc10a625fcb8414755c3af72a9
vergilx6x <med.work96@hotmail.com>
2024-07-05 15:47:58 -0700

    Cleaning up

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,12 +3,13 @@
 sends a request to the URL and displays the value
 of the X-Request-Id variable found in the header ofthe response.
 """
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
